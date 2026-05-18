bf65093bb87769bae3360f2225459cc281ff70c8
Alexander Liakouris <alexanliakou@gmail.com>
2026-05-18 13:39:03 +0300

    finished CRUD API

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
+import ideaRouter from './routes/ideaRoutes.js';
+import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//Connect to MongoDB
+connectDB();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Routes
+app.use('/api/ideas', ideaRouter);
+
+// 404 Fallback
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+})
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
